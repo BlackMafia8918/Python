f5c65d7bd27f2cb3c02b80aea24322f53567d43e
BlackMafia8918 <adesh.lpu@gmail.com>
2024-09-05 15:23:24 +0530

    Day 1 completed

---
+++ Day1.py
@@ -0,0 +1,9 @@
+print("Day 1 - Python Print Function")
+print("The Function is declared like this: ")
+print('print("What to print?")')
+
+print("Hello World!\nHello World!\nHello World!\n")
+
+print("Hello" + " " + "Black" + " " + "Mafia")
+
+print("Hello " + input("What is your name?") + "!")
