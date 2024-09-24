991ecb066e6eb7f80db3297802e1d6161b8e28d6
BlackMafia8918 <adesh.lpu@gmail.com>
2024-09-24 18:11:49 +0530

    Billing System done

---
+++ Billing.py
@@ -0,0 +1,23 @@
+print("Hello, Welcome to our Rollercoaster")
+bill = 0
+height = int(input("Enter height (in cm): "))
+
+if height >= 120:
+    print("You can ride the RollerCoaster")
+    age = int(input("Enter your age: ")) 
+    if age <= 12:
+        print("Child tickets are $5 each.")
+        bill = 5    
+    elif (age <= 18):
+        print("Youth tickets are $7 each.")
+        bill = 7
+    else:
+        print("Adult tickets are $12 each.")
+        bill = 12
+
+    wants_photo = input("Want a photo? Y or N: ")
+    if wants_photo == "Y":
+        bill += 3
+    print(f"Your total bill is: ${bill}")
+else:
+    print("Can't ride")
