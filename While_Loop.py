09e0df160c63a6044604701a17b576ded29c6c9e
BlackMafia8918 <adesh.lpu@gmail.com>
2024-10-18 18:34:58 +0530

    Functions and While Loops

---
+++ While_Loop.py
@@ -0,0 +1,28 @@
+# Reeborg's World Python Game Hurdle1 Solution
+ 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+# Method 1
+for step in range(6):
+    jump()
+
+# Method 2
+hurdles = 6
+
+while hurdles > 0:
+    jump()
+    hurdles -= 1
+    print(hurdles)
